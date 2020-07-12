42980048afb964c3434a4ad9f82dcf94484a5a38
A1337Xyz <blindwizard@tutanota.com>
2020-07-12 13:17:38 -0300

    Using hashlib to compare files

--- .scripts/Python/lsres/main.py
+++ .scripts/Python/lsres/main.py
@@ -1,31 +1,47 @@
 #!/usr/bin/env python3
 import subprocess as sp
+import hashlib
 import json
 import os
 
 cache_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cache.json')
 
+extensions = {
+    'video': ['mp4', 'avi', 'mkv', 'webm'],
+    'image': ['jpg', 'jpeg', 'png'],
+}
 try:
     with open(cache_path, 'r') as fp:
         cache = json.load(fp)
 except FileNotFoundError:
     cache = dict()
 
+BLOCK_SIZE = 64 * 1024
 for r, _, f in os.walk('.'):
     if r != '.': break
     for i in f:
-        rpath = os.path.join(os.path.realpath('.'), i)
-        if os.path.splitext(i)[-1][1:] in ['mp4', 'avi', 'mkv', 'webm']:
-            if not rpath in cache:
+        hasher = hashlib.md5()
+        with open(i, 'rb') as fp:
+            fb = fp.read(BLOCK_SIZE)
+            while len(fb) > 0:
+                hasher.update(fb)
+                fb = fp.read(BLOCK_SIZE)
+        file_hash = hasher.hexdigest()
+        ext = os.path.splitext(i)[-1][1:] 
+        if ext in extensions['video'] or ext in extensions['image']: 
+            if not file_hash in cache:
                 try:
-                    output = sp.run(['mediainfo', '--Output=Video;(%Width%, %Height%)', i], stdout=sp.PIPE).stdout
+                    if ext in  extensions['video']:
+                        output = sp.run(['mediainfo', '--Output=Video;(%Width%, %Height%)', i], stdout=sp.PIPE).stdout
+                    else:
+                        output = sp.run(['mediainfo', '--Output=Image;(%Width%, %Height%)', i], stdout=sp.PIPE).stdout
                     width, height = eval(output)
-                    cache[rpath] = (width, height) 
+                    cache[file_hash] = (width, height) 
                 except Exception as err:
                     print(err)
                     exit(1)
             else:
-                width, height = cache[rpath]
+                width, height = cache[file_hash]
             print(' {:>4}x{:<4} : {}'.format(width, height, i.split('/')[-1]))
 with open(cache_path, 'w') as fp:
     json.dump(cache, fp)
