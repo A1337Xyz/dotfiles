85a3d8e743a7307fa9da1f4dfeb6f08c14844701
A1337Xyz <blindwizard@tutanota.com>
2020-07-12 18:06:49 -0300

    improved performance

--- .scripts/Python/lsres/main.py
+++ .scripts/Python/lsres/main.py
@@ -10,6 +10,7 @@ extensions = {
     'video': ['mp4', 'avi', 'mkv', 'webm'],
     'image': ['jpg', 'jpeg', 'png'],
 }
+
 try:
     with open(cache_path, 'r') as fp:
         cache = json.load(fp)
@@ -17,18 +18,21 @@ except FileNotFoundError:
     cache = dict()
 
 BLOCK_SIZE = 64 * 1024
+
 for r, _, f in os.walk('.'):
     if r != '.': break
     for i in f:
-        hasher = hashlib.md5()
-        with open(i, 'rb') as fp:
-            fb = fp.read(BLOCK_SIZE)
-            while len(fb) > 0:
-                hasher.update(fb)
-                fb = fp.read(BLOCK_SIZE)
-        file_hash = hasher.hexdigest()
         ext = os.path.splitext(i)[-1][1:] 
         if ext in extensions['video'] or ext in extensions['image']: 
+
+            hasher = hashlib.md5()
+            with open(i, 'rb') as fp:
+                fb = fp.read(BLOCK_SIZE)
+                while len(fb) > 0:
+                    hasher.update(fb)
+                    fb = fp.read(BLOCK_SIZE)
+            file_hash = hasher.hexdigest()
+
             if not file_hash in cache:
                 try:
                     if ext in  extensions['video']:
@@ -43,6 +47,6 @@ for r, _, f in os.walk('.'):
             else:
                 width, height = cache[file_hash]
             print(' {:>4}x{:<4} : {}'.format(width, height, i.split('/')[-1]))
+
 with open(cache_path, 'w') as fp:
     json.dump(cache, fp)
-
