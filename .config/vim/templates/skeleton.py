198f4b67b15f646c5f60ef669322bd6b372ce2f6
A1337Xyz <blindwizard@tutanota.com>
2020-10-20 12:58:15 -0300

    update vim

--- .config/vim/templates/skeleton.py
+++ .config/vim/templates/skeleton.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import sys, os
+from urllib.request import Request, urlopen, urlretrieve
+import sys, os
+
+USER_AGENT = "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
+
 
 def main():
     pass
